bcd2ac3d7268c1f9300829d93d622f2d59222f73
DavidJnathani <79584778+DavidJnathani@users.noreply.github.com>
2021-10-07 06:47:48 +0300

    Update seo-meta.js

--- components/seo-meta.js
+++ components/seo-meta.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 const Meta = (props) => (      
 <Head>
-<title>Professor Munno Traditiion Healer</title>
-  
+<title>Professor Munno Traditiion Healer</title> 
 <meta name="description" content="Professor Munno Traditional Healer best in witchcraft spells and healing Call: 0200-991-807 " />
 <meta name="robots" content="noodp"/>
+ <link rel="canonical" href="https://www.professormunnotraditionalhealer.com/" />
 <meta property="og:locale" content="en_US" />
 <meta property="og:type" content="website" />
 <meta name="og:title" property="og:title" content="Prof. Munno Specialist In Healing And Witchcraft Spells - Call:0200-991-807 , Email: profmunno2021@gmail.com" />
@@ -15,8 +15,7 @@ const Meta = (props) => (
 <meta name="twitter:title" content="Call:0200-991-807 , Email: profmunno2021@gmail.com" />
 <meta name="twitter:description" content= "Prof. Munno Specialist In Healing And Witchcraft Spells - Call:0200-991-807" />
 <link rel="icon" type="image/png" href="/favicon.png" />
-<link rel="apple-touch-icon" href="/static/images/favicon.ico" />  
-<link rel="canonical" href="" />  
+<link rel="apple-touch-icon" href="/static/images/favicon.ico" />    
 {/* <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/9bc08e4d5056b57cf397be82f/5730b700e45b70f727591d1a7.js");</script>  */}
 </Head>
 )
