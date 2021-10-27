d2ca7707f64ca8704cd75a9fc3596c9626f47be7
DavidJnathani <79584778+DavidJnathani@users.noreply.github.com>
2021-10-27 17:30:28 +0300

    Update more.js

--- pages/more.js
+++ pages/more.js
@@ -9,6 +9,7 @@ import Testimonials from '../components/testimonials'
 import Banner from '../components/banner'
 import images from '../images/fog.jpg'
 import Meta from '../components/seo-meta'
+import Watsap from '../components/watsap'
 
 function More() {
 
@@ -50,6 +51,7 @@ function More() {
       <Banner page="Other Services" image={images}/>
      <Others/>
      <Testimonials/>
+          <Watsap/>
        <Footer/>
             
         </div>
